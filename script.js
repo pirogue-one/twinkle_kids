ea0eb309f18fd3ebe6974a82f62589fc40f1b7c4
aleriiu <aleriiiu@gmail.com>
2024-06-11 14:27:25 +0300

    added corrected

--- script.js
+++ script.js
@@ -36,3 +36,44 @@ mobMenu.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     modalMenu.style.display = 'none';
 })
+
+
+//header fixed 
+
+const header = document.querySelector('.header-fixed');
+const headerH = document.querySelector('.header-fixed').clientHeight;
+const navOne = document.querySelector('.nav-one');
+const navTwo = document.querySelector('.nav-two');
+const navThree = document.querySelector('.nav-three');
+const navFour = document.querySelector('.nav-four');
+const navFive = document.querySelector('.nav-five');
+const navSix = document.querySelector('.nav-six');
+
+document.onscroll = function () {
+
+    let scroll = window.scrollY;
+
+    if (scroll > headerH) {
+        header.classList.add('fixed');
+        document.body.style.paddingTop = headerH + 'px';
+        header.style.backgroundColor = '#FFFFFF';
+        navOne.style.color = '#85AFC8';
+        navTwo.style.color = '#85AFC8';
+        navThree.style.color = '#85AFC8';
+        navFour.style.color = '#85AFC8';
+        navFive.style.color = '#85AFC8';
+        navSix.style.color = '#85AFC8';
+    }
+
+    else {
+        header.classList.remove('fixed');
+        document.body.removeAttribute('style');
+        header.removeAttribute('style');
+        navOne.removeAttribute('style');
+        navTwo.removeAttribute('style');
+        navThree.removeAttribute('style');
+        navFour.removeAttribute('style');
+        navFive.removeAttribute('style');
+        navSix.removeAttribute('style');
+    }
+} 
