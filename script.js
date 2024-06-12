b4c328b47ab58aec8cdd34d3fa9aee64f478c702
aleriiu <aleriiiu@gmail.com>
2024-06-12 11:12:35 +0300

    small changes

--- script.js
+++ script.js
@@ -1,43 +1,5 @@
 'use strict';
 
-// аккордеон
-
-const accordionItems = document.querySelectorAll(".accordion-item");
-
-accordionItems.forEach(accordionItem => {
-    accordionItem.addEventListener("click", event => {
-
-        accordionItem.classList.toggle("active");
-        const accordionItemBody = accordionItem.querySelector(".accordion-body");
-        const accArrow = accordionItem.querySelector('.accordion-arrow');
-        if (accordionItem.classList.contains("active")) {
-            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-            accArrow.style.transform = "rotate(90deg)";
-        } else {
-            accordionItemBody.style.maxHeight = 0;
-            accArrow.style.transform = "rotate(0)";
-        }
-
-    });
-});
-
-// modal 
-
-//menu - hamburger
-
-const closeBtn = document.querySelector('#close');
-const modalMenu = document.querySelector('.modal_menu-container');
-const mobMenu = document.querySelector('#menu-burger');
-
-mobMenu.addEventListener('click', () => {
-    modalMenu.style.display = 'flex';
-});
-
-closeBtn.addEventListener('click', () => {
-    modalMenu.style.display = 'none';
-})
-
-
 //header fixed 
 
 const header = document.querySelector('.header-fixed');
@@ -79,3 +41,40 @@ document.onscroll = function () {
         navSix.removeAttribute('style');
     }
 } 
+
+// аккордеон
+
+const accordionItems = document.querySelectorAll(".accordion-item");
+
+accordionItems.forEach(accordionItem => {
+    accordionItem.addEventListener("click", event => {
+
+        accordionItem.classList.toggle("active");
+        const accordionItemBody = accordionItem.querySelector(".accordion-body");
+        const accArrow = accordionItem.querySelector('.accordion-arrow');
+        if (accordionItem.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+            accArrow.style.transform = "rotate(90deg)";
+        } else {
+            accordionItemBody.style.maxHeight = 0;
+            accArrow.style.transform = "rotate(0)";
+        }
+
+    });
+});
+
+// modal 
+
+//menu - hamburger
+
+const closeBtn = document.querySelector('#close');
+const modalMenu = document.querySelector('.modal_menu-container');
+const mobMenu = document.querySelector('#menu-burger');
+
+mobMenu.addEventListener('click', () => {
+    modalMenu.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', () => {
+    modalMenu.style.display = 'none';
+})
