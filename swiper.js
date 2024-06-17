630841adbb4eeacc4ad954ccec1a0beece78fb57
pirogue-one <alyona.evg@yandex.ru>
2024-06-17 20:08:45 +0300

    added gallery to swiper and other small changes

--- swiper.js
+++ swiper.js
@@ -23,3 +23,43 @@ const swiperPart = new Swiper('.swiper-partners', {
     prevEl: '.partners-button-prev',
   },
 });
+
+//модальные окна слайдера
+const docSlides = document.querySelectorAll('.swiper-slide-docs img');
+const popup = document.querySelector('.popup-image');
+const popupImage = popup.querySelector('#popup-image');
+const closePopup = document.querySelector('#popup-close');
+const arrowPrev = document.querySelector('.arrow-prev');
+const arrowNext = document.querySelector('.arrow-next');
+
+let currentIndex = 0;
+
+docSlides.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    popupImage.src = item.getAttribute('src');
+    popup.classList.toggle('popup-hidden');
+    currentIndex = index;
+  });
+})
+
+closePopup.addEventListener('click', () => {
+  popup.classList.toggle('popup-hidden');
+})
+
+arrowPrev.addEventListener('click', () => {
+  if (currentIndex === 0) {
+    currentIndex = docSlides.length - 1;
+  } else {
+    currentIndex--;
+  }
+  popupImage.src = docSlides[currentIndex].getAttribute('src');
+})
+
+arrowNext.addEventListener('click', () => {
+  if (currentIndex === docSlides.length - 1) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+  popupImage.src = docSlides[currentIndex].getAttribute('src');
+})
